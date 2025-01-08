bbaaa46dc66aa1954dec99808d7533292085ea2e
ffineapple <imffineapple@gmail.com>
2025-01-08 19:35:47 +0900

    [250108-2] 2752 세수정렬

--- boj/2752.cpp
+++ boj/2752.cpp
@@ -2,21 +2,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int input1, input2, input3;
+int first, second, third;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    vector<int> v(3);
-    int MIN = 1000001, MAX = 0;
-    for(int i = 0; i < 3; i++) {
-        cin >> v[i];
-        if(v[i] < MIN) MIN = v[i];
-        if(v[i] > MAX) MAX = v[i];
-    }
+    cin >> input1 >> input2 >> input3;
 
-    cout << MIN << " ";
-    for(int e : v) {
-        if(e != MIN && e != MAX) cout << e << " ";
-    }
-    cout << MAX;
+    first = min({input1, input2, input3});
+    third = max({input1, input2, input3});
+    second = (input1 + input2 + input3) - first - third;
+    
+    cout << first << " " << second << " " << third;
 }
